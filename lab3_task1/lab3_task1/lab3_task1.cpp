2828b833aa0155cec452fca8038229eb435df441
Александр <96746458+AlexCode61@users.noreply.github.com>
2024-03-18 01:09:23 +0300

    Add task

---
+++ lab3_task1/lab3_task1/lab3_task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+
+	cout << "Введите строку\n";
+	getline(cin, str);
+	for (int i = 0; i < (str.size()); i++) {
+		if ((str[i] == ' ') && (str[i + 1] == ' ')) {
+			str.erase(i + 1, 1);
+			i = i - 1;
+		}
+	}
+	cout << "Изменённая строка:";
+	cout << str;
+	return 0;
+}
---
+++ lab3_task2/lab3_task2/lab3_task2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string str;
+    int count = 0;
+
+    char mas[22] = { 'А', 'У', 'О', 'Ы', 'И', 'Э', 'Я', 'Ю', 'Ё', 'Е', 'а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е' };
+
+    cout << "Введите строку\n";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ') {
+            for (int j = 0; j < 10; j++) {
+                if (str[i + 1] == mas[j]) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    cout << "Количество слов, начинающихся с гласной: \n";
+    cout << count;
+
+    return 0;
+}
